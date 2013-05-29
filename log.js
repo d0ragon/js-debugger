ad9caea2d530cc79ed9747afc5d2c46f4f852300
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-05-29 17:52:26 +0400

    Create log.js

---
+++ log.js
@@ -0,0 +1,35 @@
+window.debug = {
+  enabled: true,
+  log: function ()
+  {
+    if (!this.enabled)
+    {
+      return;
+    }
+    var sc = [];
+    for (var i = 0, num = Array.prototype.slice.call(arguments).length; i < num; i ++)
+    {
+      sc.push('arguments[' + i + ']');
+    //sc += 'arguments[' + i + '], ';
+    }
+    sc = sc.join(', ');
+  //sc = sc.replace(/,\s$/, '');
+    eval('console.log(' + sc + ')');
+  },
+  on: function ()
+  {
+    this.enabled = true;
+  },
+  off: function ()
+  {
+    this.enabled = false;
+  },
+  enable: function ()
+  {
+    return this.on();
+  },
+  disable: function ()
+  {
+    return this.off();
+  }
+};
